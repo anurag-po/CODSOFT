d214dbe3e12423dad7c32f4265d974c9320d427f
anurag-po <anuragpo393@gmail.com>
2025-12-13 21:37:55 +0530

    Add health check endpoint to server

--- job-board/server/index.js
+++ job-board/server/index.js
@@ -45,3 +45,6 @@ app.post('/api/notify', async (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
